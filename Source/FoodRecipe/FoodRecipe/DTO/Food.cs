adce7a2837ed55dcec88412baf3376674a43adaf
khanhhongtranle <khanhhong141049@gmail.com>
2020-10-22 15:41:08 +0700

    update add food screens
    Screens/AddFoodScreens/
    +AddGenaralInfo screen
    +AddSteps screen

---
+++ Source/FoodRecipe/FoodRecipe/DTO/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.DTO
+{
+    class Food
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //hinh anh thanh qua
+
+        public FoodStep[] steps;
+    }
+
+    class FoodStep
+    {
+        public string DescriptionStep { get; set; }
+        //danh sach hinh anh minh hoa
+        public string VideoStepLink { get; set; }
+    }
+}
