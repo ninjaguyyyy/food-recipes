088e79b4990b7aa7ed0cdd94ef84cb6232dcc849
a3chi <ninjaguyyyy@gmail.com>
2020-10-28 12:21:54 +0700

    refresh project + splash screen + list food

---
+++ Source/FoodRecipe/FoodRecipe/DAO/FoodDAO.cs
@@ -0,0 +1,31 @@
+﻿using FoodRecipe.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.DAO
+{
+    class FoodDAO
+    {
+        public static BindingList<Food> GetAll()
+        {
+            var result = new BindingList<Food>()
+            {
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 1"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+                new Food() {ThumbnailPath="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg", Name="Food 2"},
+            };
+            return result;
+        }
+    }
+}
---
+++ Source/FoodRecipe/FoodRecipe/Screens/ListFood.xaml.cs
@@ -0,0 +1,39 @@
+﻿using FoodRecipe.DAO;
+using FoodRecipe.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FoodRecipe.Screens
+{
+    /// <summary>
+    /// Interaction logic for ListFood.xaml
+    /// </summary>
+    public partial class ListFood : Window
+    {
+        private BindingList<Food> _list = new BindingList<Food>();
+
+        public ListFood()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _list = FoodDAO.GetAll();
+            dataListView.ItemsSource = _list;
+
+        }
+    }
+}
