23f15d3553f882defe73c2e7bf7ca329e554302f
a3chi <ninjaguyyyy@gmail.com>
2020-10-29 15:45:20 +0700

    done paging for list food

--- Source/FoodRecipe/FoodRecipe/DAO/FoodDAO.cs
+++ Source/FoodRecipe/FoodRecipe/DAO/FoodDAO.cs
@@ -12,20 +12,46 @@ namespace FoodRecipe.DAO
 {
     class FoodDAO
     {
-        public static BindingList<Food> GetAll()
+        public static int GetLengthAll()
+        {
+            var result = 0;
+
+            XDocument xdocument = XDocument.Load("../../Db/DB.xml");
+            IEnumerable<XElement> foodsElements = xdocument.Root.Elements();
+            result = foodsElements.Count();
+
+            return result;
+        }
+
+        public static BindingList<Food> GetAll(int perPage, int page)
         {
             var result = new BindingList<Food>();
 
+            int skipValue = page == 1 ? 0 : (page - 1) * perPage;
+
+
             XDocument xdocument = XDocument.Load("../../Db/DB.xml");
-            IEnumerable<XElement> foods = xdocument.Root.Elements();
-            foreach (var foodEl in foods)
+            IEnumerable<XElement> foodsElements = xdocument.Root.Elements().Skip(skipValue).Take(perPage);
+
+
+
+            result = ConvertListXmlElementToFoods(foodsElements);
+
+            return result;
+        }
+
+        public static BindingList<Food> ConvertListXmlElementToFoods(IEnumerable<XElement> listElement)
+        {
+            var result = new BindingList<Food>();
+
+            foreach (var xelement in listElement)
             {
                 var foodItem = new Food();
-                foodItem.Id = foodEl.Element("id").Value;
-                foodItem.Name = foodEl.Element("name").Value;
-                foodItem.Description = foodEl.Element("description").Value;
-                foodItem.ThumbnailPath = foodEl.Element("thumbnailPath").Value;
-                foodItem.IsFavorite = Boolean.Parse(foodEl.Element("isFavorite").Value);
+                foodItem.Id = xelement.Element("id").Value;
+                foodItem.Name = xelement.Element("name").Value;
+                foodItem.Description = xelement.Element("description").Value;
+                foodItem.ThumbnailPath = xelement.Element("thumbnailPath").Value;
+                foodItem.IsFavorite = Boolean.Parse(xelement.Element("isFavorite").Value);
 
                 result.Add(foodItem);
             }
